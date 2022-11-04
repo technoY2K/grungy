4cfe5065c3f828f3a82a213fdb35f7cc0edc79ad
Kevia Cloud <keviacloud@gamestop.com>
2022-11-04 15:15:32 -0700

    chore: remove extra application files - bare bones

---
+++ app/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, describe, expect } from "vitest";
+
+describe("An example test", () => {
+    test("true is true, unless you're crazy", () => {
+        expect(true).toBe(true);
+    });
+});
