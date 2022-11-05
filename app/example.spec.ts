f8d33fba5efecbbe62a0e98abe796b4328ccb10d
Kevia Cloud <keviacloud@gamestop.com>
2022-11-04 19:08:21 -0700

    fix: remove merge conflict marker

--- app/example.spec.ts
+++ app/example.spec.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import { test, describe, expect } from "vitest";
 
 describe("An example test", () => {
     test("true is true, unless you're crazy", () => {
-=======
-import { describe, it } from "vitest";
-
-describe("Example test", () => {
-    it("should pass", () => {
->>>>>>> fc1eba3 (test: add example test)
         expect(true).toBe(true);
     });
 });
