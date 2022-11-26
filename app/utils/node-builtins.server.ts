fb3d88557eafcbdbef98862da1f553d8a166e68c
Kevia Cloud <keviacloud@gamestop.com>
2022-11-26 14:58:30 -0700

    chore(deps): update to imx core-sdk

--- app/utils/node-builtins.server.ts
+++ app/utils/node-builtins.server.ts
@@ -1,17 +1,16 @@
 // These are for dependencies that bring server code into the client bundle
 // https://remix.run/docs/en/v1/pages/gotchas
 
-import { ImmutableXClient } from "@imtbl/imx-sdk";
+import { ImmutableX, Config } from "@imtbl/core-sdk";
 
 export const imxClient = async () => {
-    const url = "https://api.x.immutable.com/v1";
-    const client = await ImmutableXClient.build({ publicApiUrl: url });
+    const client = new ImmutableX(Config.SANDBOX);
 
     const getAsset = async () => {
         const response = await client.getAsset({
-            address: "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
-            id: "214063224",
-            include_fees: false,
+            tokenAddress: "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c",
+            tokenId: "214063224",
+            includeFees: false,
         });
 
         return response;
